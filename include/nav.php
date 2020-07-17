d8a88397afec5609746144ade84737b8bc7fa874
aniket2102 <36763702+aniket2102@users.noreply.github.com>
2020-07-17 18:51:16 +0530

    Header Update

---
+++ include/nav.php
@@ -0,0 +1,66 @@
+
+<!--Navbar Starts-->
+<div class="fixed-top">
+  <!--1st Nav Starts-->
+  <ul class="navbar bg-light navbar-expand-lg nav nav-fill">
+    <li class="nav-item">
+       <a class="navbar-brand" href="#"><img src="assets/images/logo.png"></a>
+    </li>  
+    <li class="nav-item">
+      <a class="nav-link text-primary" href="#"><i class="fa fa-envelope ">info@chromatus.com </i></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-primary" href="tel:7498465144"><i class="fa fa-phone">+91-7498465144</i></a>
+    </li>
+    <div class="nav justify-content-center"> 
+    <li class="nav-item">
+      <a class="nav-link text-primary" href="https://www.linkedin.com/company/chromatusconsulting" target="_blank"><i class="fa fa-linkedin-square  fa-2x"></i></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-primary" href="https://www.facebook.com/Chromatus-2467152553553218" target="_blank"><i class="fa fa-facebook-official  fa-2x"></i></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-primary" href="https://twitter.com/Chromatus12" target="_blank"><i class="fa fa-twitter-square  fa-2x"></i></a>
+    </li>
+  </div>
+    <li class="nav-item ">
+      <a href="#" class="nav-link btn btn-primary" >Register/Login</a> 
+    </li>
+  </ul>
+  <!--1st Nav Ends-->
+  <!--2nd Nav Starts-->
+  <div class="navbar navbar-dark  bg-dark navbar-expand-lg ">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar navbar-nav navbar-collapse">
+     <li class="nav-item">
+       <a class="nav-link" href="#">Why US</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="#">Press Release</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="#">Blogs</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="#">Industries</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="#">Contact Us</a>
+     </li>
+     <li class="nav-item">
+     <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <a href="#" class="btn btn-outline-primary my-2 my-sm-0 justify-content-center " type="submit"><i class="fa fa-search"></i></a>
+    </form>
+     </li>
+    </ul>
+  </div>
+  </div>
+  <!--2nd Nav Ends-->
+</div>
+<!--Navbar Ends-->
+<!--avoid-overflow is to avoid Content going under fixed navbar-->
+<div class="avoid-overflow"></div>
--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-To be editted.
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">    
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+    
+    <title></title>
+    </head>
+    <body>
+      <section class="header">
+        <?php require_once('include/nav.php'); ?>    
+      </section>
+      <div class="main-content">
+        
+      </div>
+
+     
+      
+
+
+
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="assets/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
