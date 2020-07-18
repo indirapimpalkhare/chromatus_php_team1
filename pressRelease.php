a8d28dd35b9bd59a6a88cb55ded45b71365222f7
Kartikeya Pande <45434534+kartikeya0611@users.noreply.github.com>
2020-07-19 01:39:58 +0530

    Add files via upload

---
+++ pressRelease.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Press Release</title>
+
+<!--bootstrap css-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+<!--custom css-->
+<link rel="stylesheet" href="css/style.css">
+
+<?php require_once('include/header.php'); ?>
+
+</head>
+<body>
+
+<section class="header">
+        <?php require_once('include/nav.php'); ?>
+</section>
+
+    <div class="checkFloat" >
+       
+        <div class="left">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                      <th>Industry</th>
+                    </tr>
+                </thead>
+                <tr>
+                    <td> Industry Info </td>
+                </tr>
+            </table>
+            
+        </div> 
+           
+        
+        <div class="right border border-dark">
+            <h2>Press Release</h2>
+            <table class="table table-striped table-bordered">
+                <tr>
+                    <td>PR1</td>
+                </tr>
+                <tr>
+                    <td>PR2</td>
+                </tr>
+                <tr>
+                    <td>PR3</td>
+                </tr>
+                <tr>
+                    <td>PR4</td>
+                </tr>
+                <tr>
+                    <td>PR5</td>
+                </tr>
+                <tr>
+                    <td>PR6</td>
+                </tr>
+                
+            </table>
+        </div>      
+    </div>
+    <?php require_once('include/footer.php'); ?>
+
+
+</body>
+</html>
