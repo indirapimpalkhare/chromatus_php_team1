5bc0b67d38eddf82586dd48923c959dd5d526624
komal raut <komalrautkkw@gmail.com>
2020-07-19 19:44:36 +0530

    Update pressRelease.php

--- pressRelease.php
+++ pressRelease.php
@@ -11,23 +11,37 @@
         <div class = "row ">
             <div class="col-md-4 align-self-start ">
                 <table class="table table-bordered ">
-                    <thead>
+                    <thead class="thead-light">
+		
                         <tr>
                           <th style="text-align:center;" >Industry</th>
                         </tr>
                     </thead>
+					<tbody>
                     <tr>
                         <td> Industry Info </td>
                     </tr>
+					</tbody>
                 </table>
             </div>
 
     
             <div class="col-md-8 align-self-start border border-dark">
-                <h2 style="text-align:center;" >Press Release</h2>
+                <h2 style="text-align:center;" >Press Release</h2><br>
+                
+	
+                <div class="card">
+                    <h5 class="card-header text-white" style="background-color:#1aa3ff;">PR-1Heading</h5>
+					<div class="card-body text-black">
+                        <h5 class="card-title">Date</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    
+					</div>
+                </div>
                 
                 <div class="card">
-                    <h5 class="card-header">PR1-Heading</h5>
+                    <h5 class="card-header text-white" style="background-color:#1aa3ff;">PR2-Heading</h5>
                     <div class="card-body">
                         <h5 class="card-title">Date</h5>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -36,7 +50,7 @@
                 </div>
                 
                 <div class="card">
-                    <h5 class="card-header">PR2-Heading</h5>
+                    <h5 class="card-header text-white" style="background-color:#1aa3ff;">PR3-Heading</h5>
                     <div class="card-body">
                         <h5 class="card-title">Date</h5>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -45,7 +59,7 @@
                 </div>
                 
                 <div class="card">
-                    <h5 class="card-header">PR3-Heading</h5>
+                    <h5 class="card-header text-white" style="background-color:#1aa3ff;">PR4-Heading</h5>
                     <div class="card-body">
                         <h5 class="card-title">Date</h5>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -54,7 +68,7 @@
                 </div>
                 
                 <div class="card">
-                    <h5 class="card-header">PR4-Heading</h5>
+                    <h5 class="card-header text-white" style="background-color:#1aa3ff;">PR5-Heading</h5>
                     <div class="card-body">
                         <h5 class="card-title">Date</h5>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -63,16 +77,7 @@
                 </div>
                 
                 <div class="card">
-                    <h5 class="card-header">PR5-Heading</h5>
-                    <div class="card-body">
-                        <h5 class="card-title">Date</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-                
-                <div class="card">
-                    <h5 class="card-header">PR6-Heading</h5>
+                    <h5 class="card-header text-white" style="background-color:#1aa3ff;">PR6-Heading</h5>
                     <div class="card-body">
                         <h5 class="card-title">Date</h5>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
