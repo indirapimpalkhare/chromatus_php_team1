0f825ee7983d3f446ceaa1ce12788538f9159057
Indira Pimpalkhare <indira.pimpalkhare@gmail.com>
2020-07-19 09:42:44 +0530

    structure change

--- pressRelease.php
+++ pressRelease.php
@@ -1,26 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Press Release</title>
-
-<!--bootstrap css-->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-<!--custom css-->
-<link rel="stylesheet" href="css/style.css">
-
 <?php require_once('include/header.php'); ?>
+  <body>
+    <section class="header">
+      <?php require_once('include/nav.php'); ?>
+    </section>
+    <div class="main-content">
 
-</head>
-<body>
-
-<section class="header">
-        <?php require_once('include/nav.php'); ?>
-</section>
 
     <div class="checkFloat" >
-       
+
         <div class="left">
             <table class="table table-bordered">
                 <thead>
@@ -32,10 +21,10 @@
                     <td> Industry Info </td>
                 </tr>
             </table>
-            
-        </div> 
-           
-        
+
+        </div>
+
+
         <div class="right border border-dark">
             <h2>Press Release</h2>
             <table class="table table-striped table-bordered">
@@ -57,10 +46,11 @@
                 <tr>
                     <td>PR6</td>
                 </tr>
-                
+
             </table>
-        </div>      
+        </div>
     </div>
+  </div>
     <?php require_once('include/footer.php'); ?>
 
 
