f07c9d0ea0943a42ba8cfdd9b9c00f3f4b18824a
aniket2102 <36763702+aniket2102@users.noreply.github.com>
2020-07-20 23:15:06 +0530

    Updated Nav

--- include/nav.php
+++ include/nav.php
@@ -1,40 +1,36 @@
 
 <!--Navbar Starts-->
 <div class="fixed-top">
-  <!--1st Nav Starts-->  
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarstop" aria-controls="navbarstop" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand ml-5 mr-3" href="index.php"><img class="w-75" src="assets/images/logo.png"></a>
-    <div class="container justify-content-center">
-    <div class="collapse navbar-collapse" id="navbarstop">
-      <div class="container">
-  	    <ul class="navbar-nav mr-auto">
-  	        <li class="nav-item text-primary ml-5 mr-5">
-  	           <a class="nav-link text-primary" href="mailto:info@chromatus.com"><i class="fa fa-envelope ">info@chromatus.com </i></a>
-  	        </li>
-  	        <li class="nav-item text-primary ml-5 mr-5">
-  	          <a class="nav-link text-primary" href="tel:7498465144"><i class="fa fa-phone">+91-7498465144</i></a>
-  	        </li>
-  	        <div class="d-flex ml-5 mr-5">
-		   	    <li class="nav-item">
-		   	      <a class="nav-link text-primary mx-sm-2" href="https://www.linkedin.com/company/chromatusconsulting" target="_blank"><i class="fa fa-linkedin-square  fa-2x"></i></a>
-		   	    </li>
-		   	    <li class="nav-item ">
-		   	      <a class="nav-link text-primary mx-sm-2" href="https://www.facebook.com/Chromatus-2467152553553218" target="_blank"><i class="fa fa-facebook-official  fa-2x"></i></a>
-		   	    </li>
-		   	    <li class="nav-item ">
-		   	      <a class="nav-link text-primary mx-sm-2" href="https://twitter.com/Chromatus12" target="_blank"><i class="fa fa-twitter-square  fa-2x"></i></a>
-		   	    </li>
-	   		</div>
-  		</ul>
-        <a href="#" class="text-white btn btn-primary mr-5">Register/Login</a>
-  		</div>
-    </div>
-    </div>
-  </nav>
 
+      <ul class="navbar navbar-light bg-light navbar-expand-lg nav nav-fill">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+       <a class="navbar-brand" href="index.php"><img class="w-75" src="assets/images/logo.png"></a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent1">
+        <li class="nav-item">
+          <a class="nav-link text-primary" href="mailto:info@chromatus.com"><i class="fa fa-envelope ">info@chromatus.com </i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-primary" href="tel:7498465144"><i class="fa fa-phone">+91-7498465144</i></a>
+        </li>
+        <div class="nav justify-content-center">
+          <li class="nav-item">
+            <a class="nav-link text-primary" href="https://www.linkedin.com/company/chromatusconsulting" target="_blank"><i class="fa fa-linkedin-square  fa-2x"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-primary" href="https://www.facebook.com/Chromatus-2467152553553218" target="_blank"><i class="fa fa-facebook-official  fa-2x"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-primary" href="https://twitter.com/Chromatus12" target="_blank"><i class="fa fa-twitter-square  fa-2x"></i></a>
+          </li>
+        </div>
+        <li class="nav-item  ">
+          <a href="#" class="nav-link btn btn-primary " >Register/Login</a>
+        </li>
+      </div>
+      </ul>
   <!--1st Nav Ends-->
   
   <!--2nd Nav Starts-->
