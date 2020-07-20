98066340dea52f1a90914ceb3339eb3b45da0e8e
aniket2102 <36763702+aniket2102@users.noreply.github.com>
2020-07-20 18:52:58 +0530

    Nav size reduced and horizontal scroll removed

--- include/nav.php
+++ include/nav.php
@@ -1,90 +1,91 @@
 
 <!--Navbar Starts-->
 <div class="fixed-top">
-  <!--1st Nav Starts-->
-
-  <ul class="navbar navbar-light bg-light navbar-expand-lg nav nav-fill">
-  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
+  <!--1st Nav Starts-->  
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarstop" aria-controls="navbarstop" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-  </button>  
-    <li class="nav-item">
-       <a class="navbar-brand" href="index.php"><img src="assets/images/logo.png"></a>
-    </li>
-  	<div class="collapse navbar-collapse" id="navbarSupportedContent1">
-    <li class="nav-item">
-      <a class="nav-link text-primary" href="mailto:info@chromatus.com"><i class="fa fa-envelope ">info@chromatus.com </i></a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link text-primary" href="tel:7498465144"><i class="fa fa-phone">+91-7498465144</i></a>
-    </li>
-    <div class="nav justify-content-center">
-	    <li class="nav-item">
-	      <a class="nav-link text-primary" href="https://www.linkedin.com/company/chromatusconsulting" target="_blank"><i class="fa fa-linkedin-square  fa-2x"></i></a>
-	    </li>
-	    <li class="nav-item">
-	      <a class="nav-link text-primary" href="https://www.facebook.com/Chromatus-2467152553553218" target="_blank"><i class="fa fa-facebook-official  fa-2x"></i></a>
-	    </li>
-	    <li class="nav-item">
-	      <a class="nav-link text-primary" href="https://twitter.com/Chromatus12" target="_blank"><i class="fa fa-twitter-square  fa-2x"></i></a>
-	    </li>
-  	</div>
-    <li class="nav-item ">
-      <a href="#" class="nav-link btn btn-primary" >Register/Login</a>
-    </li>
-	</div>
-  </ul>
+    </button>
+    <a class="navbar-brand ml-5" href="index.php"><img class="w-75" src="assets/images/logo.png"></a>
+    <div class="container justify-content-center">
+    <div class="collapse navbar-collapse" id="navbarstop">
+      <div class="container">
+  	    <ul class="navbar-nav mr-auto">
+  	        <li class="nav-item text-primary ml-5">
+  	           <a class="nav-link text-primary" href="mailto:info@chromatus.com"><i class="fa fa-envelope ">info@chromatus.com </i></a>
+  	        </li>
+  	        <li class="nav-item text-primary ml-5 pl-5">
+  	          <a class="nav-link text-primary" href="tel:7498465144"><i class="fa fa-phone">+91-7498465144</i></a>
+  	        </li>
+  	        <div class="d-flex ml-5 pl-5">
+		   	    <li class="nav-item">
+		   	      <a class="nav-link text-primary" href="https://www.linkedin.com/company/chromatusconsulting" target="_blank"><i class="fa fa-linkedin-square  fa-2x"></i></a>
+		   	    </li>
+		   	    <li class="nav-item ">
+		   	      <a class="nav-link text-primary" href="https://www.facebook.com/Chromatus-2467152553553218" target="_blank"><i class="fa fa-facebook-official  fa-2x"></i></a>
+		   	    </li>
+		   	    <li class="nav-item ">
+		   	      <a class="nav-link text-primary" href="https://twitter.com/Chromatus12" target="_blank"><i class="fa fa-twitter-square  fa-2x"></i></a>
+		   	    </li>
+	   		</div>
+	   		<li class="nav-item ml-5 pl-5 mt-1">
+             	<a href="#" class="text-white btn btn-primary ml-5">Register/Login</a>
+	   		</li>
+  		</ul>
+  		</div>
+        
+        
+      
+  </div>
+    </div>
+  </nav>
+
   <!--1st Nav Ends-->
+  
   <!--2nd Nav Starts-->
-  <div class="navbar navbar-dark  bg-dark navbar-expand-lg">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-  	<div class="container">
-	  	<ul class="navbar navbar-nav navbar-collapse">
-		
-		    <li class="nav-item">
-		       <a class="nav-link" href="#">Why US</a>
-		    </li>
-
-		    <li class="nav-item">
-		       <a class="nav-link" href="PressRelease.php">Press Release</a>
-		    </li>
-
-		    <li class="nav-item">
-		       <a class="nav-link" href="#">Blogs</a>
-		    </li>
-
-		     
-		    <li class="nav-item dropdown">
-		       <a class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Industries</a>
-		       <div class="dropdown-menu bg-dark ">
-		             <a class="dropdown-item text-white small" href="#">Category1</a>
-		             <div class="dropdown-divider"></div>
-		             <a class="dropdown-item text-white small" href="#">Category2</a>
-		             <div class="dropdown-divider"></div>
-		             <a class="dropdown-item text-white small" href="#">Category3</a>
-		             <div class="dropdown-divider"></div>
-		             <a class="dropdown-item text-white small" href="#">Category4</a>
-		             <div class="dropdown-divider "></div>
-		             <a class="dropdown-item text-white small" href="#">Category5</a>
-		        </div>
-		    </li>
-
-		    <li class="nav-item">
-		       <a class="nav-link" href="#">Contact Us</a>
-		    </li>
-		
-		    <li class="nav-item ">
-		     	<form class="form-inline my-2 my-lg-0">
-		        	<input class="form-control mr-sm-2 float-left" type="search" placeholder="Search" aria-label="Search">
-		        	<a href="#" class="btn btn-outline-primary my-2 my-sm-0  " type="submit"><i class="fa fa-search"></i></a>
-		    	</form>
-		    </li>
-		</ul>
-	</div>
-  </div>
-  </div>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarsExample05">
+      <div class="container ">
+      <ul class="navbar-nav position-relative>
+        <li class="nav-item mr-5">
+          <a class="nav-link text-white" href="#">Why Us!</a>
+        </li>
+        <li class="nav-item mr-5">
+          <a class="nav-link text-white" href="PressRelease.php">Press Release</a>
+        </li>
+        <li class="nav-item mr-5">
+          <a class="nav-link text-white" href="#">Blogs</a>
+        </li>
+       <li class="nav-item  mr-5 dropdown">
+	       <a class="nav-link text-white dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Industries</a>
+	       <div class="dropdown-menu bg-dark ">
+	             <a class="dropdown-item text-white small" href="#">Category1</a>
+	             <div class="dropdown-divider"></div>
+	             <a class="dropdown-item text-white small" href="#">Category2</a>
+	             <div class="dropdown-divider"></div>
+	             <a class="dropdown-item text-white small" href="#">Category3</a>
+	             <div class="dropdown-divider"></div>
+	             <a class="dropdown-item text-white small" href="#">Category4</a>
+	             <div class="dropdown-divider "></div>
+	             <a class="dropdown-item text-white small" href="#">Category5</a>
+	        </div>
+	    </li>
+        <li class="nav-item mr-5">
+          <a class="nav-link text-white" href="#">Contact Us!</a>
+        </li>
+      </ul>
+      <form class="form-inline">
+	     <input class="form-control mr-sm-2 " type="search" placeholder="Search" aria-label="Search">
+	    <a href="#" class="btn btn-outline-primary my-2 my-sm-0  " type="submit"><i class="fa fa-search"></i></a>
+	  </form>
+      </div>
+    </div>
+  </nav>
+  
   <!--2nd Nav Ends-->
 </div>
 <!--Navbar Ends-->
--- index.php
+++ index.php
@@ -10,30 +10,31 @@
       <div class="main-content">
 
 
-
-        <div class = "row pt-4 mt-4">
-          <div id="carouselHomePage" class="carousel slide carousel-fade" data-ride="carousel" style="height:200px;">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="assets/images/placeholder1.jpg" class="w-100" style="height:200px;" alt="...">
+        <div class="container-fluid">
+          <div class = "row">
+            <div id="carouselHomePage" class="carousel slide carousel-fade" data-ride="carousel" style="height:200px;">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="assets/images/placeholder1.jpg" class="w-100" style="height:200px;" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/images/placeholder2.jpg" class="w-100" style="height:200px;" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="assets/images/placeholder3.jpg" class="w-100" style="height:200px;" alt="...">
+                </div>
               </div>
-              <div class="carousel-item">
-                <img src="assets/images/placeholder2.jpg" class="w-100" style="height:200px;" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="assets/images/placeholder3.jpg" class="w-100" style="height:200px;" alt="...">
-              </div>
+              <a class="carousel-control-prev" href="#carouselHomePage" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselHomePage" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
             </div>
-            <a class="carousel-control-prev" href="#carouselHomePage" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselHomePage" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
           </div>
-        </div>
+      </div>
         <div class = "row m-4 pt-4 mt-4">
           <div class = "col-md-8 align-self-start">
             <div class="card">
