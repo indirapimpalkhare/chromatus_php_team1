b46ba15bd697c4f0596662ca643f9ebf81b4e9d5
aniket2102 <36763702+aniket2102@users.noreply.github.com>
2020-07-21 12:41:44 +0530

    Indira Check once

--- include/nav.php
+++ include/nav.php
@@ -1,14 +1,14 @@
 
 <!--Navbar Starts-->
 <div class="fixed-top">
-
+    
       <ul class="navbar navbar-light bg-light navbar-expand-lg nav nav-fill">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
            <span class="navbar-toggler-icon"></span>
         </button>
         <li class="nav-item"> <a class="navbar-brand" href="index.php"><img class="w-75" src="assets/images/logo.png"></a></li>
-
-
+       
+      
         <div class="collapse navbar-collapse" id="navbarSupportedContent1">
         <li class="nav-item">
           <a class="nav-link text-primary" href="mailto:info@chromatus.com"><i class="fa fa-envelope ">info@chromatus.com </i></a>
@@ -33,26 +33,26 @@
       </div>
       </ul>
   <!--1st Nav Ends-->
-
+  
   <!--2nd Nav Starts-->
-
+  
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarsExample05">
-      <!--div class="container "-->
-      <ul class="navbar-nav position-relative">
-        <li class="nav-item mr-5">
+      <div class="container ">
+      <ul class="navbar-nav ">
+        <li class="nav-item mr-4">
           <a class="nav-link text-white" href="#">Why Us!</a>
         </li>
-        <li class="nav-item mr-5">
+        <li class="nav-item mr-4">
           <a class="nav-link text-white" href="PressRelease.php">Press Release</a>
         </li>
-        <li class="nav-item mr-5">
+        <li class="nav-item mr-4">
           <a class="nav-link text-white" href="#">Blogs</a>
         </li>
-       <li class="nav-item  mr-5 dropdown">
+       <li class="nav-item mr-4 dropdown">
 	       <a class="nav-link text-white dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Industries</a>
 	       <div class="dropdown-menu bg-dark ">
 	             <a class="dropdown-item text-white bg-dark small" href="#">Category1</a>
@@ -66,7 +66,7 @@
 	             <a class="dropdown-item text-white bg-dark small" href="#">Category5</a>
 	        </div>
 	    </li>
-        <li class="nav-item ">
+        <li class="nav-item mr-4">
           <a class="nav-link text-white" href="#">Contact Us!</a>
         </li>
       </ul>
@@ -76,8 +76,9 @@
 	    <a href="#" class=" form-control btn btn-outline-primary my-2 my-sm-0  " type="submit"><i class="fa fa-search"></i></a>
 	  </form>
       </div>
+    </div>
   </nav>
-
+  
   <!--2nd Nav Ends-->
 </div>
 <!--Navbar Ends-->
