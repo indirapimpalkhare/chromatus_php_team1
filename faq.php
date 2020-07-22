5093a028e22a627268e450d239ccedb9c5c24a6a
Indira Pimpalkhare <indira.pimpalkhare@gmail.com>
2020-07-22 13:42:18 +0530

    FAQs

---
+++ faq.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <?php require_once('include/header.php'); ?>
+
+    <body>
+      <header>
+        <?php require_once('include/nav.php'); ?>
+      </header>
+
+
+      <div class="main-content" style="padding-top:60px;">
+        <div class="container pb-4">
+          <h1 class="text-center"><u> FAQ's </u></h1>
+        <div class="accordion" id="accordionExample">
+  <div class="card mb-1">
+    <div class="card-header h-30" id="headingOne">
+      <h2 class="mb-0">
+        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Question #1
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="card mb-1">
+    <div class="card-header" id="headingTwo">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Question #2
+        </button>
+      </h2>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="card mb-1">
+    <div class="card-header" id="headingThree">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Question #3
+        </button>
+      </h2>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+    <?php require_once('include/footer.php'); ?>
+  </body>
+</html>
--- pressRelease.php
+++ pressRelease.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php require_once('include/header.php'); ?>
-  
+
   <body>
     <section class="header">
       <?php require_once('include/nav.php'); ?>
     </section>
-    
-    <div class="main-content">
+
+    <div class="main-content" style="padding-top:60px;">
 
     <div  class="container " >
         <div class = "row ">
             <div class="col-md-4 align-self-start ">
                 <table class="table table-bordered ">
                     <thead class="thead-light">
-		
+
                         <tr>
                           <th style="text-align:center;" >Industry</th>
                         </tr>
@@ -27,21 +27,21 @@
                 </table>
             </div>
 
-    
+
             <div class="col-md-8 align-self-start border border-dark">
                 <h2 style="text-align:center;" >Press Release</h2><br>
-                
-	
+
+
                 <div class="card">
                     <h5 class="card-header text-white" style="background-color:#1aa3ff;">PR-1Heading</h5>
 					<div class="card-body text-black">
                         <h5 class="card-title">Date</h5>
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
-                    
+
 					</div>
                 </div>
-                
+
                 <div class="card">
                     <h5 class="card-header text-white" style="background-color:#1aa3ff;">PR2-Heading</h5>
                     <div class="card-body">
@@ -50,7 +50,7 @@
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
-                
+
                 <div class="card">
                     <h5 class="card-header text-white" style="background-color:#1aa3ff;">PR3-Heading</h5>
                     <div class="card-body">
@@ -59,7 +59,7 @@
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
-                
+
                 <div class="card">
                     <h5 class="card-header text-white" style="background-color:#1aa3ff;">PR4-Heading</h5>
                     <div class="card-body">
@@ -68,7 +68,7 @@
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
-                
+
                 <div class="card">
                     <h5 class="card-header text-white" style="background-color:#1aa3ff;">PR5-Heading</h5>
                     <div class="card-body">
@@ -77,7 +77,7 @@
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
-                
+
                 <div class="card">
                     <h5 class="card-header text-white" style="background-color:#1aa3ff;">PR6-Heading</h5>
                     <div class="card-body">
@@ -86,14 +86,14 @@
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
-                
+
             </div>
         </div>
     </div>
-    
+
     </div>
-    
-    
+
+
 
 
 
