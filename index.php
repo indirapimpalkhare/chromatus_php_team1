8f92cbda044c478940ecef049c461fffaa4c94c0
Indira Pimpalkhare <indira.pimpalkhare@gmail.com>
2020-08-15 14:48:12 +0530

    our services

--- index.php
+++ index.php
@@ -10,7 +10,67 @@
             min-width: 100%;
             height: auto;
             max-height: inherit;
-
+        }
+        .our_services{
+          margin: 0;
+          padding: 0;
+          font-family: "montserrat",sans-serif;
+        }
+        .services{
+          background: #f1f1f1;
+          text-align: center;
+        }
+        .services h1{
+          display: inline-block;
+          text-transform: uppercase;
+          border-bottom: 4px solid #1c5e80;
+          font-size: 30px;
+          padding-bottom: 10px;
+          margin-top: 40px;
+        }
+        .cen{
+          max-width: 1200px;
+          margin: auto;
+          overflow: hidden;
+          padding: 10px;
+        }
+        .service{
+          display: inline-block;
+          width: calc(100% / 3);
+          margin: 0 -5px;
+          padding: 30px;
+          box-sizing: border-box;
+          cursor: pointer;
+          transition: 0.4s;
+        }
+        .service:hover{
+          background: #ddd;
+        }
+        .service i{
+          color: #1c5e80;
+          font-size: 34px;
+          margin-bottom: 30px;
+        }
+        .service h2{
+          font-size: 18px;
+          text-transform: uppercase;
+          font-weight: 500;
+          margin: 0;
+        }
+        .service p{
+          color: gray;
+          font-size: 15px;
+          font-weight: 500;
+        }
+        @media screen and (max-width: 800px) {
+          .service{
+            width: 50%;
+          }
+        }
+        @media screen and (max-width: 500px) {
+          .service{
+            width: 100%;
+          }
         }
     </style>
 <html lang="en">
@@ -20,9 +80,6 @@
         <!--Header Start-->
         <?php require_once('include/nav.php'); ?>
         <!--Header Ends-->
-
-
-
 
         <div class = "imgbox">
           <div id="carouselHomePage" class="carousel slide carousel-fade" data-ride="carousel">
@@ -47,7 +104,8 @@
             </a>
           </div>
         </div>
-        <div class="main-content">
+
+        <div class="our_services">
         <div class = "row m-4 pt-4 mt-4">
           <div class = "col-md-8 align-self-start">
             <div class="card">
@@ -74,35 +132,68 @@
             </div>
           </div>
         </div>
-
-        <div class="container-fluid text-center">
-          <hr>
-          <h2>Services</h2>
-
          <br>
-         <div class="container">
-          <div class="row">
-            <div class="col-sm-4" align="center">
-              <img src="assets/images/Widespread_Reach.png" class="img-responsive center" style="width:30% " alt="Widespread Reach">
-              <h4>Widespread Reach</h4>
-              <p>Leverage our global online press release distribution network to reach readers outside the circle of your regular audiences. Our platform promises a broader global reach with distribution across quality databases and websites.</p>
+         <div class="services">
+            <h1>Our Services</h1>
+            <div class="cen">
+              <div class="service">
+                <i class="fa fa-newspaper-o" aria-hidden = "true"></i>
+                <h2>Annual Subscription</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              </div>
 
-            </div>
-            <div class="col-sm-4" align="center">
-               <img src="assets/images/Hefty_ROI.png" class="img-responsive center"  style="width:30% " alt="Hefty ROI">
-               <h4>Hefty ROI</h4>
-              <p>Besides trust and brand visibility, you can easily build or improve your customer base as your press releases get published over applicable news platforms and receive the right media coverage. We provide you the best returns! </p>
+              <div class="service">
+                <i class="fab fa-android"></i>
+                <h2>Competitive Benchmarking</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              </div>
 
+              <div class="service">
+                <i class="fa fa-user-circle-o"></i>
+                <h2>CSR Consulting</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              </div>
+
+              <div class="service">
+                <i class="fas fa-apple-alt"></i>
+                <h2>Customer Satisfaction Surveys</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              </div>
+
+              <div class="service">
+                <i class="fa fa-globe"></i>
+                <h2>Customized Consulting</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              </div>
+
+              <div class="service">
+                <i class="fa fa-line-chart"></i>
+                <h2>Data Mining &amp; Analytics</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              </div>
+
+              <div class="service">
+                <i class="far fa-angry"></i>
+                <h2>Field Survey</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              </div>
+
+              <div class="service">
+                <i class="fa fa-book"></i>
+                <h2>Knowledge Warehouse</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              </div>
+
+              <div class="service">
+                <i class="fa fa-file-text-o"></i>
+                <h2>Lead Generation</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              </div>
             </div>
-            <div class="col-sm-4" align="center">
-               <img src="assets/images/Lucrative_Leads.png" class="img-responsive center"  style="width:30% " alt="Lucrative Leads">
-               <h4>Lucrative Leads</h4>
-              <p>Press release syndication is one of the crucial elements of digital marketing in today’s age of internet. Our result-oriented services increase the effectiveness of your marketing and lead generation strategies.</p>
-           </div>
           </div>
         </div>
-        </div>
       </div>
+    </div>
 
     <?php require_once('include/footer.php'); ?>
 
