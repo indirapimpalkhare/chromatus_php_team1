b1e6fe8c9a289827be56c5c07d4cd035cd95896d
Indira Pimpalkhare <indira.pimpalkhare@gmail.com>
2020-08-30 17:48:34 +0530

    Header + Title

--- faq.php
+++ faq.php
@@ -1,7 +1,17 @@
 <!doctype html>
 <html lang="en">
-  <?php require_once('include/header.php'); ?>
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+
+        <title>FAQ - Chromatus Consulting</title>
+    </head>
     <body>
       <header>
         <?php require_once('include/nav.php'); ?>
--- index.php
+++ index.php
@@ -73,9 +73,19 @@
           }
         }
     </style>
-<html lang="en">
-  <?php require_once('include/header.php'); ?>
+  <html lang="en">
+      <head>
+          <!-- Required meta tags -->
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+          <!-- Bootstrap CSS -->
+          <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+          <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+          <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+
+          <title>Home - Chromatus Consulting</title>
+      </head>
     <body>
         <!--Header Start-->
         <?php require_once('include/nav.php'); ?>
