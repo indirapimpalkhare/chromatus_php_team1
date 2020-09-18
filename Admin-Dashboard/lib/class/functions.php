4c12db729fb6670a00a254ebdab0fffc708ac125
kartikeya0611 <kartikeya7pande7@gmail.com>
2020-09-19 02:20:12 +0530

    pr-function-1

--- Admin-Dashboard/lib/class/functions.php
+++ Admin-Dashboard/lib/class/functions.php
@@ -905,4 +905,429 @@
 		} */
 
 		// ---- BLOGS END ---- //
+
+
+
+
+
+
+
+// ---- PRESS RELEASE STARTS HERE ---- //
+
+
+
+function add_pr_category($pr_category)
+			{
+				if($stmt_insert = $this->con->prepare("INSERT INTO `pr_category`(`name`) VALUES (?)"))
+				{
+					$stmt_insert->bind_param("s",$pr_category);
+
+					if($stmt_insert->execute())
+					{
+						return true;
+					}
+					return false;
+				}
+			}
+			function fetch_pr_category()
+			{
+				if($stmt_select = $this->con->prepare("SELECT `prCategoryID`,`name` FROM `pr_category` where `status` = 1"))
+				{
+					$stmt_select->bind_result($category_id,$category_name);
+
+					if($stmt_select->execute())
+					{
+						$data = array();
+						$counter	=	0;
+
+						while($stmt_select->fetch())
+						{
+							$data[$counter][0] = $category_id;
+							$data[$counter][1] = $category_name;
+							$counter++;
+						}
+						if(!empty($data))
+						{
+							return $data;
+						}
+						else
+						{
+							return false;
+						}
+					}
+				}
+			}
+			function fetch_pr_deleted_category()
+			{
+				if($stmt_select = $this->con->prepare("SELECT `prCategoryID`,`name` FROM `pr_category` where `status` = 0"))
+				{
+					$stmt_select->bind_result($category_id,$category_name);
+
+					if($stmt_select->execute())
+					{
+						$data = array();
+						$counter	=	0;
+
+						while($stmt_select->fetch())
+						{
+							$data[$counter][0] = $category_id;
+							$data[$counter][1] = $category_name;
+							$counter++;
+						}
+						if(!empty($data))
+						{
+							return $data;
+						}
+						else
+						{
+							return false;
+						}
+					}
+				}
+			}
+			function delete_pr_category_by_id($delete_id)
+			{
+				if($stmt_update = $this->con->prepare("UPDATE `pr_category` SET `status` = 0 where `prCategoryID` = ?"))
+				{
+					$stmt_update->bind_param("s",$delete_id);
+
+					if($stmt_update->execute())
+					{
+					return true;
+					}
+					else
+					{
+					return false;
+					}
+				}
+			}
+
+
+			function permanent_delete_pr_category_by_id($delete_id)
+			{
+				if($stmt_delete = $this->con->prepare("DELETE FROM `pr_category` where `prCategoryID` = ?"))
+				{
+					$stmt_delete->bind_param("i",$delete_id);
+
+					if($stmt_delete->execute())
+					{
+						return false;
+					}
+				}
+			}
+
+			function restore_deleted_pr_category_by_id($restore_id)
+			{
+				if($stmt_update = $this->con->prepare("UPDATE `pr_category` SET `status` = 1 where `prCategoryID` = ?"))
+				{
+					$stmt_update->bind_param("s",$restore_id);
+
+					if($stmt_update->execute())
+					{
+					return true;
+					}
+					else
+					{
+					return false;
+					}
+				}
+			}
+            
+            function update_pr_category_by_id($category_name,$category_id)
+			{
+				if($stmt_update = $this->con->prepare("UPDATE `pr_category` SET `name`= ? where `prCategoryID`= ?"))
+				{
+					$stmt_update->bind_param("ss",$category_name,$category_id);
+
+					if($stmt_update->execute())
+					{
+					return true;
+					}
+					else
+					{
+					return false;
+					}
+				}
+			}
+            
+            function fetch_pr_category_by_id($category_id)
+			{
+				if($stmt_select = $this->con->prepare("SELECT `name` FROM `pr_category` where `prCategoryID` = ?"))
+				{
+					$stmt_select->bind_param("s",$category_id);
+					$stmt_select->bind_result($category_name);
+
+					if($stmt_select->execute())
+					{
+						$data = array();
+
+
+						while($stmt_select->fetch())
+						{
+							$data[0] = $category_name;
+						}
+						if(!empty($data))
+						{
+							return $data;
+						}
+						else
+						{
+							return false;
+						}
+					}
+				}
+			}
+
+/*
+function add_news($news_title,$news_category,$news_metadesc,$news_desc,$news_permalink)
+			{
+				$date = date("Y-m-d");
+
+				if($stmt_insert = $this->con->prepare("INSERT INTO `news`(`title`, `category`, `metaDescription`, `description`, `permalink`, `date`) VALUES (?,?,?,?,?,?)"))
+				{
+					$stmt_insert->bind_param("ssssss",$news_title,$news_category,$news_metadesc,$news_desc,$news_permalink,$date);
+
+					if($stmt_insert->execute())
+					{
+						return true;
+					}
+					return false;
+				}
+			}
+
+			function fetch_news_records()
+			{
+				if($stmt_select = $this->con->prepare("SELECT `newsID`,`title`, `category`, `metaDescription`, `description`, `permalink`, `date` FROM `news` where `status` = 1"))
+				{
+					$stmt_select->bind_result($news_id,$news_title,$news_category,$news_metadesc,$news_desc,$news_permalink,$date);
+
+					if($stmt_select->execute())
+					{
+						$data = array();
+						$counter	=	0;
+
+						while($stmt_select->fetch())
+						{
+							$data[$counter][0] = $news_id;
+							$data[$counter][1] = $news_title;
+							$data[$counter][2] = $news_category;
+							$data[$counter][3] = $news_metadesc;
+							$data[$counter][4] = $news_desc;
+							$data[$counter][5] = $news_permalink;
+							$data[$counter][6] = $date;
+
+							$counter++;
+						}
+						if(!empty($data))
+						{
+							return $data;
+						}
+						else
+						{
+							return false;
+						}
+					}
+				}
+			}
+			function fetch_news_deleted_records()
+			{
+				if($stmt_select = $this->con->prepare("SELECT `newsID`,`title`, `category`, `metaDescription`, `description`, `permalink`, `date` FROM `news` where `status` = 0"))
+				{
+					$stmt_select->bind_result($news_id,$news_title,$news_category,$news_metadesc,$news_desc,$news_permalink,$date);
+
+					if($stmt_select->execute())
+					{
+						$data = array();
+						$counter	=	0;
+
+						while($stmt_select->fetch())
+						{
+							$data[$counter][0] = $news_id;
+							$data[$counter][1] = $news_title;
+							$data[$counter][2] = $news_category;
+							$data[$counter][3] = $news_metadesc;
+							$data[$counter][4] = $news_desc;
+							$data[$counter][5] = $news_permalink;
+							$data[$counter][6] = $date;
+
+							$counter++;
+						}
+						if(!empty($data))
+						{
+							return $data;
+						}
+						else
+						{
+							return false;
+						}
+					}
+				}
+			}
+			function delete_news_details_by_id($delete_id)
+			{
+				if($stmt_update = $this->con->prepare("UPDATE `news` SET `status` = 0 where `newsID` = ?"))
+				{
+					$stmt_update->bind_param("s",$delete_id);
+
+					if($stmt_update->execute())
+					{
+					return true;
+					}
+					else
+					{
+					return false;
+					}
+				}
+			}
+			function permanent_delete_news_details_by_id($delete_id)
+			{
+				if($stmt_delete = $this->con->prepare("delete FROM `news` where `newsId` = ?"))
+				{
+					$stmt_delete->bind_param("i",$delete_id);
+
+					if($stmt_delete->execute())
+					{
+						return false;
+					}
+				}
+			}
+			function restore_deleted_news_details_by_id($restore_id)
+			{
+				if($stmt_update = $this->con->prepare("UPDATE `news` SET `status` = 1 where `newsID` = ?"))
+				{
+					$stmt_update->bind_param("s",$restore_id);
+
+					if($stmt_update->execute())
+					{
+					return true;
+					}
+					else
+					{
+					return false;
+					}
+				}
+			}
+
+			
+            
+			function update_news_full_details_by_id($news_title,$news_category,$news_metadesc,$news_desc,$news_permalink,$news_id)
+			{
+				if($stmt_update = $this->con->prepare("UPDATE `news` SET `title`= ? ,`category`= ? ,`metaDescription`=  ? ,`description`= ? ,`permalink`=? where `newsID`= ? "))
+				{
+					$stmt_update->bind_param("ssssss",$news_title,$news_category,$news_metadesc,$news_desc,$news_permalink,$news_id);
+
+					if($stmt_update->execute())
+					{
+					return true;
+					}
+					else
+					{
+					return false;
+					}
+				}
+			}
+			
+			function fetch_news_full_details_by_id($news_id)
+			{
+				if($stmt_select = $this->con->prepare("SELECT `newsID`,`title`, `category`, `metaDescription`, `description`, `permalink`, `date` FROM `news` where `newsID` = ?"))
+				{
+					$stmt_select->bind_param("s",$news_id);
+					$stmt_select->bind_result($news_id,$news_title,$news_category,$news_metadesc,$news_desc,$news_permalink,$date);
+
+					if($stmt_select->execute())
+					{
+						$data = array();
+
+
+						while($stmt_select->fetch())
+						{
+							$data[0] = $news_id;
+							$data[1] = $news_title;
+							$data[2] = $news_category;
+							$data[3] = $news_metadesc;
+							$data[4] = $news_desc;
+							$data[5] = $news_permalink;
+							$data[6] = $date;
+
+						}
+						if(!empty($data))
+						{
+							return $data;
+						}
+						else
+						{
+							return false;
+						}
+					}
+				}
+			}
+			function fetch_news_category_by_id($category_id)
+			{
+				if($stmt_select = $this->con->prepare("SELECT `name` FROM `news_category` where `newsCategoryID` = ?"))
+				{
+					$stmt_select->bind_param("s",$category_id);
+					$stmt_select->bind_result($category_name);
+
+					if($stmt_select->execute())
+					{
+						$data = array();
+
+
+						while($stmt_select->fetch())
+						{
+							$data[0] = $category_name;
+						}
+						if(!empty($data))
+						{
+							return $data;
+						}
+						else
+						{
+							return false;
+						}
+					}
+				}
+			}
+			//frontend code...
+			function fetch_news_records_by_name($category)
+			{
+				if($stmt_select = $this->con->prepare("SELECT `newsID`,`title`, `category`, `metaDescription`, `description`, `permalink`, `date` FROM `news` where `status` = 1 AND category = ?"))
+				{
+					$stmt_select->bind_param("s",$category);
+					$stmt_select->bind_result($news_id,$news_title,$news_category,$news_metadesc,$news_desc,$news_permalink,$date);
+
+					if($stmt_select->execute())
+					{
+						$data = array();
+						$counter	=	0;
+
+						while($stmt_select->fetch())
+						{
+							$data[$counter][0] = $news_id;
+							$data[$counter][1] = $news_title;
+							$data[$counter][2] = $news_category;
+							$data[$counter][3] = $news_metadesc;
+							$data[$counter][4] = $news_desc;
+							$data[$counter][5] = $news_permalink;
+							$data[$counter][6] = $date;
+
+							$counter++;
+						}
+						if(!empty($data))
+						{
+							return $data;
+						}
+						else
+						{
+							return false;
+						}
+					}
+				}
+			}
+
+
+
+*/
+
+    // ---- Press release Ends here ----  //
 ?>
