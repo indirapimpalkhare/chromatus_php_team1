991fb889bf6a1558d3da0a19ead1efa5c21f057b
Indira Pimpalkhare <indira.pimpalkhare@gmail.com>
2020-09-21 15:39:47 +0530

    link

--- Admin-Dashboard/include/dashboard-left-part.php
+++ Admin-Dashboard/include/dashboard-left-part.php
@@ -160,7 +160,7 @@
                </a>
                <ul class="pcoded-submenu">
                   <li class="">
-                     <a href="#">
+                     <a href="manage_home.php">
                      <span class="pcoded-micon"><i class="ti-angle-right"></i></span>
                      <span class="pcoded-mtext">Manage</span>
                      <span class="pcoded-mcaret"></span>
