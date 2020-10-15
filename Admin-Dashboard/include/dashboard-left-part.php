037a6255ba326b7d12819a0816b9ba6ef0b6e0ae
aniket2102 <36763702+aniket2102@users.noreply.github.com>
2020-10-15 19:25:02 +0530

    Backend User Manual

--- Admin-Dashboard/include/dashboard-left-part.php
+++ Admin-Dashboard/include/dashboard-left-part.php
@@ -2,7 +2,7 @@
 <nav class="pcoded-navbar">
    <div class="sidebar_toggle"><a href="#"><i class="icon-close icons"></i></a></div>
    <div class="pcoded-inner-navbar main-menu">
-      <div class="pcoded-navigation-label">Navigation</div>
+     
       <ul class="pcoded-item pcoded-left-item">
          <li class="active">
             <a href="dashboard.php">
@@ -241,30 +241,6 @@
                      </a>
                   </li>
                </ul>
-         </li>
-         
-         <li class="pcoded-hasmenu ">
-            <a href="javascript:void(0)">
-            <span class="pcoded-micon"><i class="ti-gallery"></i><b>G</b></span>
-            <span class="pcoded-mtext">User</span>
-            <span class="pcoded-mcaret"></span>
-            </a>
-            <ul class="pcoded-submenu">
-               <li class="">
-                  <a href="add-#">
-                  <span class="pcoded-micon"><i class="ti-angle-right"></i></span>
-                  <span class="pcoded-mtext">Add User</span>
-                  <span class="pcoded-mcaret"></span>
-                  </a>
-               </li>
-               <li class="">
-                  <a href="#">
-                  <span class="pcoded-micon"><i class="ti-angle-right"></i></span>
-                  <span class="pcoded-mtext">Manage User</span>
-                  <span class="pcoded-mcaret"></span>
-                  </a>
-               </li>
-            </ul>
          </li>
    </div>
 </nav>
